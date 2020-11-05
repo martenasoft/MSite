0cec1f1019729bdd51d1fbd031d16f668303b1d9
kanisimov <kanisimov@xmlshop.com>
2020-11-05 16:55:29 +0200

    Remove old nested sets libroary

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,10 +2,6 @@
 
 namespace MartenaSoft\Site\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use MartenaSoft\Common\Entity\NestedSetEntityInterface;
-use MartenaSoft\Menu\Entity\Menu;
-use MartenaSoft\Menu\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
