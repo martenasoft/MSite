7a202bd011f121f3914833f4af32d9d8f43bdc84
kanisimov <kanisimov@xmlshop.com>
2020-12-24 18:30:02 +0200

    Media Bundle

--- src/DependencyInjection/MartenaSoftSiteExtension.php
+++ src/DependencyInjection/MartenaSoftSiteExtension.php
@@ -15,41 +15,19 @@ class MartenaSoftSiteExtension extends Extension implements PrependExtensionInte
 {
     public function prepend(ContainerBuilder $container): void
     {
-
-      /*  $container->prependExtensionConfig(MartenaSoftMediaBundle::class, [
+/*
+        $container->prependExtensionConfig(MartenaSoftMediaBundle::getConfigName(), [
             'images_path' => [
-                'articles' => [
+                [
                     'dir' => 'articles',
-                    'small' => [
-                        'width' => 120,
-                        'height' => 120,
-                        'path' => 'small'
-                    ]
+                      'small' => [
+                          'width' => 120,
+                          'height' => 120,
+                          'path' => 'small'
+                      ]
                 ]
             ]
         ]);*/
-        // get all bundles
-      /*  $bundles = $container->getParameter('kernel.bundles');
-
-        // determine if AcmeGoodbyeBundle is registered
-        if (!isset($bundles['AcmeGoodbyeBundle'])) {
-            // disable AcmeGoodbyeBundle in bundles
-            $config = ['use_acme_goodbye' => false];
-            foreach ($container->getExtensions() as $name => $extension) {
-                switch ($name) {
-                    case 'acme_something':
-                    case 'acme_other':
-                        // set use_acme_goodbye to false in the config of
-                        // acme_something and acme_other
-                        //
-                        // note that if the user manually configured
-                        // use_acme_goodbye to true in config/services.yaml
-                        // then the setting would in the end be true and not false
-                        $container->prependExtensionConfig($name, $config);
-                        break;
-                }
-            }
-        }*/
     }
 
 
@@ -62,7 +40,7 @@ class MartenaSoftSiteExtension extends Extension implements PrependExtensionInte
 
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(dirname(__DIR__).'/Resources/config')
+            new FileLocator(dirname(__DIR__) . '/Resources/config')
         );
         $loader->load('services.yaml');
     }
